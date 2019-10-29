1d02951eb3ed13f0dab7699c088f7f6a7e61c55b
Taiying Chen <taiying.tychen@gmail.com>
2019-10-28 19:09:15 -0500

    HW4: Move function to class

--- HW4/recommender.py
+++ HW4/recommender.py
@@ -39,6 +39,62 @@ def read_input(filename):
     return ratings, movies, queries
 
 
+class Recommender(object):
+    def __init__(self, ratings):
+        self.user_ratings = defaultdict(lambda: defaultdict(float))
+        self.movie_ratings = defaultdict(lambda: defaultdict(float))
+        self.movie_biases = {}
+        self.user_biases = {}
+        self.movie_similarities = defaultdict(lambda: defaultdict(float))
+
+        for user, movie, rating in ratings:
+            self.user_ratings[user][movie] = rating
+            self.movie_ratings[movie][user] = rating
+
+        self.global_mean = self.get_global_mean(ratings)
+
+
+    def get_global_mean(self, ratings):
+        total_ratings = []
+        for user, movie, rating in ratings:
+            total_ratings.append(rating)
+        return sum(total_ratings) / len(total_ratings)
+
+
+    def estimate_movie_biases(self):
+        for movie in self.movie_ratings:
+            # b_i = sum(r_ui - mu) / len(R(i))
+            movie_bias = [rating - self.global_mean for rating in self.movie_ratings[movie].values()]
+            movie_bias = sum(movie_bias) / len(movie_bias)
+            self.movie_biases[movie] = movie_bias
+
+
+    def estimate_user_biases(self):
+        for user in self.user_ratings:
+            # b_u = sum(r_ui - mu - b_i) / len(R(u))
+            user_bias = [rating - self.global_mean - self.movie_biases[movie] for movie, rating in self.user_ratings[user].items()]
+            user_bias = sum(user_bias) / len(user_bias)
+            self.user_biases[user] = user_bias
+
+        
+    def baseline_predictor(self, user, movie):
+        """Baseline predictor (b_ui)
+        """
+        return self.user_biases[user] + self.movie_biases[movie] + self.global_mean
+
+    
+    def predict(self, user, movie):
+        bias = self.baseline_predictor(user, movie)
+
+        similarity = 0.
+        total_similarity = 0.
+        for user_movie in self.user_ratings[user]:
+            similarity += self.movie_similarities[movie][user_movie] * (self.user_ratings[user][user_movie] - self.baseline_predictor(user, user_movie))
+            total_similarity += self.movie_similarities[movie][user_movie]
+        
+        return bias + similarity / total_similarity
+    
+
 def output_result(output, filename):
     with open(filename, 'w') as file:
         for row in output:
@@ -52,32 +108,10 @@ def main(argv):
 
     ratings, movies, queries = read_input(input_filename)
     
+    recommender = Recommender(ratings)
+    recommender.estimate_movie_biases()
+    recommender.estimate_user_biases()
     
-    user_ratings = defaultdict(list)
-    movie_ratings = defaultdict(list)
-    global_mean = []
-    for user, movie, rating in ratings:
-        user_ratings[user].append((movie, rating))
-        movie_ratings[movie].append((user, rating))
-        global_mean.append(rating)
-    global_mean = sum(global_mean) / len(ratings)
-
-    # Estimate movie biases
-    movie_biases = {}
-    for movie in movie_ratings:
-        # b_i = sum(r_ui - mu) / len(R(i))
-        movie_bias = [rating - global_mean for user, rating in movie_ratings[movie]]
-        movie_bias = sum(movie_bias) / len(movie_bias)
-        movie_biases[movie] = movie_bias
-    
-    # Estimate user biases
-    user_biases = {}
-    for user in user_ratings:
-        # b_u = sum(r_ui - mu - b_i) / len(R(u))
-        user_bias = [rating - global_mean - movie_biases[movie] for movie, rating in user_ratings[user]]
-        user_bias = sum(user_bias) / len(user_bias)
-        user_biases[user] = user_bias
-
     pass
 
 
